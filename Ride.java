a1c820a5cb6211194773ac44d42a7d13a897c969
Ashu Pant <108819915+ashupant-cs22@users.noreply.github.com>
2022-07-20 19:06:44 +0530

    ADDED : Rides

---
+++ Ride.java
@@ -0,0 +1,20 @@
+package com.bridgelabz;
+
+public class Ride {
+
+    public double distance;
+    public int time;
+
+    public Ride(double distance, int time) {
+        this.distance = distance;
+        this.time = time;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public int getTime() {
+        return time;
+    }
+}
